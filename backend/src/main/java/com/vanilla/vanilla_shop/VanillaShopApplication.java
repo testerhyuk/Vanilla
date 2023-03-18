af278b8277a26d0774327ba433154c3126479864
Hyuk <79625509+testerhyuk@users.noreply.github.com>
2023-03-18 13:53:59 +0900

    front back 폴더 구분

---
+++ backend/src/main/java/com/vanilla/vanilla_shop/VanillaShopApplication.java
@@ -0,0 +1,13 @@
+package com.vanilla.vanilla_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanillaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanillaShopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vanilla/vanilla_shop/VanillaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanilla.vanilla_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanillaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
